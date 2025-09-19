7517d1ff4b478170c6017bd485ead3405eb82ae5
rupeshpanchal07759 <panchalrupesh18@gmail.com>
2025-09-19 14:14:43 +0530

    First commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import "./config/instrument.js";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 
@@ -20,11 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Routes
-app.get("/", (req, res) => res.send("Api working"));
+app.get("/", (req, res) => res.send("API Working"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-
 app.post("/webhooks", clerkWebhooks);
 
 // Sentry error handler should come **after all routes**
