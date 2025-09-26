5fb1bbaefc4c36843230432912aa624c6d77858f
rupeshpanchal07759 <panchalrupesh18@gmail.com>
2025-09-26 16:35:08 +0530

    Fourth commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,37 @@
+// db.js
 import mongoose from "mongoose";
 
-// Function to connect to MongoDB database
-const connectDB = async () => {
-  mongoose.connection.on("connected", () =>
-    console.log("MongoDB connected successfully")
-  );
+const MONGODB_URI = process.env.MONGODB_URI;
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
-};
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env");
+}
+
+// Use a global variable to cache the connection
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    // Return cached connection if it exists
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    // Create a new connection promise
+    cached.promise = mongoose
+      .connect(`${MONGODB_URI}/job-portal`)
+      .then((mongoose) => {
+        console.log("MongoDB connected successfully");
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
